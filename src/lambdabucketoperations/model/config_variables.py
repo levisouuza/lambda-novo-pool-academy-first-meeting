8d276f21cd776729eca20b19cf5f7c6ad18fdb8a
levisouuza <levisouuza@gmail.com>
2025-12-10 11:36:30 -0300

    refactor: :fire: implementing sqs service

--- src/lambdabucketoperations/model/config_variables.py
+++ src/lambdabucketoperations/model/config_variables.py
@@ -2,6 +2,7 @@ from pydantic import BaseSettings
 
 
 class ConfigVariables(BaseSettings):
-    glue_job_name: str
-    worker_type: str
-    number_of_workers: int
+    glue_job_name: str | None
+    worker_type: str | None
+    number_of_workers: int | None
+    queue_url_sqs: str
